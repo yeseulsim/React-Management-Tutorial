1297a0624aff6331691c97f7ee5b60062c808b70
yeseulsim <minisomi31@gmail.com>
2021-07-13 01:24:21 +0900

    Create React Project

---
+++ management/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  return() {
+    return (
+      <div className="gray-background">
+        <img src={logo} lat="logo" />
+        <h2>let's develop management system!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
